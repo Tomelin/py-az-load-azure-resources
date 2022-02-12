e878b6bffbf301b55cbc8d3fc6edaefa770a2444
Tomelin <rafael.tomelin@gmail.com>
2022-02-12 18:39:23 -0300

    Update elastic.py

--- elastic.py
+++ elastic.py
@@ -9,7 +9,7 @@ es = Elasticsearch("http://elasticsearch:9200")
 
 r = requests.get('http://elasticsearch:9200')
 i = 1
-while r.status_code == 5:
+while r.status_code == 200:
     r = requests.get('https://swapi.dev/api/people/'+ str(i))
     es.index(index='sw',  id=i, body=json.loads(r.content))
     i=i+1
@@ -20,7 +20,7 @@ es.get(index='sw', id=5)
 
 r = requests.get('http://elasticsearch:9200')
 i = 18
-while r.status_code == 30:
+while r.status_code == 200:
    r = requests.get('https://swapi.dev/api/people/'+ str(i))
    es.index(index='sw', doc_type='people', id=i,     body=json.loads(r.content))
    i=i+1
